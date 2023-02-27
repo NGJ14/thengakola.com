410e394470ba4344981045867a465bb132bd5ca9
NAVEEN GEORGE JOSEPH <naveengjoseph@gmail.com>
2023-02-27 11:10:28 +0530

    loader

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ $(".fa-chevron-down").click(function () {
   $(this).parent().closest("div").siblings(".middle").toggleClass("hidden");
   $(this).parent().closest("div").siblings(".middle").slideToggle(400);
 });
+
+
+
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
