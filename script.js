8712c7ee789f2877ab8b499451a348d12d231f96
ro-jc <rohan.rj120@gmail.com>
2023-02-27 10:11:45 +0530

    spaces

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ $(".fa-chevron-down").click(function () {
   $(this).toggleClass("rotate");
 });
 
-
-
 $(".fa-chevron-down").click(function () {
   $(this).parent().closest("div").siblings(".middle").toggleClass("hidden");
   $(this).parent().closest("div").siblings(".middle").slideToggle(400);
