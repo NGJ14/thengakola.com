ac2be32d90ef54bb0f5cb17e4e5e2537220a246c
NAVEEN GEORGE JOSEPH <naveengjoseph@gmail.com>
2023-02-27 23:02:45 +0530

    openingScreen

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+$(".logo").dblclick(function () {
+  var step1 = 1;
+  var step3 = 0;
+  setInterval(function () {
+    if ($(".logo").css("opacity") >= "0") {
+      step1 -= 0.04;
+      $(".logo").css("opacity", `${step1}`);
+    }
+    if ($(".logo").css("opacity") <= "0") {
+      step3 += 0.03;
+      $(".main").css("opacity", `${step3}`);
+      $(".appbar").css("opacity", `${step3}`);
+    }
+
+  }, 30);
+
+  var step3 = 0;
+
+  setTimeout(function () {
+    setInterval(function () {
+      if ($(".logo").css("opacity") <= "0") {
+        step3 += 0.04;
+        $(".main").css("opacity", `${step3}`);
+      }
+      $(".appbar").css("opacity", `${step3}`);
+    }, 50);
+    $(".logo").css("display", "none");
+  }, 999);
+});
+
 $(".fa-chevron-down").click(function () {
   $(this).toggleClass("rotate");
 });
@@ -6,15 +36,3 @@ $(".fa-chevron-down").click(function () {
   $(this).parent().closest("div").siblings(".middle").toggleClass("hidden");
   $(this).parent().closest("div").siblings(".middle").slideToggle(400);
 });
-
-
-
-document.onreadystatechange = function () {
-  if (document.readyState !== "complete") {
-    document.querySelector("body").style.visibility = "hidden";
-    document.querySelector("#loader").style.visibility = "visible";
-  } else {
-    document.querySelector("#loader").style.display = "none";
-    document.querySelector("body").style.visibility = "visible";
-  }
-};
