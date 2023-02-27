a06506312c7b701aa6746dd2d5ba1be7c54e5915
ro-jc <rohan.rj120@gmail.com>
2023-02-27 11:14:29 +0530

    get json file

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ $(".fa-chevron-down").click(function () {
   $(this).parent().closest("div").siblings(".middle").toggleClass("hidden");
   $(this).parent().closest("div").siblings(".middle").slideToggle(400);
 });
+
+const now = new Date(Date.now());
+
+function getJson(name) {
+  fetch('./timetables/' + name + '.json')
+    .then((response) => {return JSON.parse(response)});
+}
+
+function setCurrent(name) {
+  periodsToday = getJson(name)[(now.getDay() - 1) % 7];
+}
