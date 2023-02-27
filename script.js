697f035eb1bc1de2772ab586b0389b74aa99b2d7
ro-jc <rohan.rj120@gmail.com>
2023-02-27 14:34:47 +0530

    add upcoming classes to table

--- script.js
+++ script.js
@@ -8,35 +8,67 @@ $(".fa-chevron-down").click(function () {
 });
 
 
-function getPeriodsToday(name, day) {
+function setData(name, day) {
   fetch('./timetables/' + name + '.json')
     .then((response) => response.json())
-    .then((json) => writeData(json[day]));
+    .then((json) => writeData(name, json[day]));
 }
 
-function writeData(periodsToday) {
-  // console.log(periodsToday);
+function createTimeString(timings) {
+  let timeString = '';
+  if (timings.length > 0) {
+    timeString += timings[0].toString().padStart(2, '0') + ':';
+    timeString += timings[1].toString().padStart(2, '0') + '-';
+    timeString += timings[2].toString().padStart(2, '0') + ':';
+    timeString += timings[3].toString().padStart(2, '0');
+  }
+  return timeString;
+}
 
-  var current = {
+function writeData(name, periodsToday) {
+  var period = {
     'title': 'No more today, enjoy!',
     'venue': '',
     'timings': []
   };
 
-  for (period of periodsToday) {
-    let timings = period['timings'];
-    // console.log(period);
-    if (now.getHours() >= timings[0] && now.getMinutes() > timings[1]) {
-      // console.log('in 1')
-      if (now.getHours() <= timings[2] && now.getMinutes() < timings[3]) {
-        current = period;
-        break;
+  // Find ongoing period
+  for (var i = periodsToday.length-1; i>=0; i--) {
+    let current_period = periodsToday[i];
+    let timings = current_period['timings'];
+
+    let start = new Date(now.getFullYear(), now.getMonth(), now.getDate(), timings[0], timings[1], 0);
+    let end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), timings[2], timings[3], 0);
+
+    if (end < now) {
+      break
+    } else {
+      if (start <= now) {
+        period = current_period;
       }
     }
   }
 
-  console.log(current);
+  // Set class title
+  $('.' + name).find('.subName')[0].innerHTML = period['title'];
+
+  // Set class timings
+  $('.' + name).find('.time')[0].innerHTML = createTimeString(period['timings']);
+
+  // Set class location
+  $('.' + name).find('.location')[0].innerHTML = period['venue'];
+
+  // Set upcoming classes
+  var table = $('.' + name).find('table')[0];
+  for (i+=2;i<periodsToday.length;i++) {
+    next_period = periodsToday[i];
+    table.innerHTML += `<tr>
+      <td>${createTimeString(next_period['timings'])}</td>
+      <td>${next_period['venue']}</td>
+      <td>${next_period['title']}</td>
+    </tr>`
+  }
 }
 
 const now = new Date(Date.now());
-getPeriodsToday('annie', 0);
+setData('yami', 0);
